bc209f0f7700f114cea87e80cfe295cca1eb3ee9
SuryoSC <suryos265@gmail.com>
2025-04-24 13:23:23 +0700

    wlululu

--- pasien/forgot-password.php
+++ pasien/forgot-password.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Log in</title>
+    <title>AdminLTE 3 | Forgot Password</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
@@ -20,14 +20,14 @@
 <body class="hold-transition login-page">
     <div class="login-box">
         <div class="login-logo">
-            <a href="../../index2.html"><b>User</b>Login</a>
+            <a href="../index2.html"><b>Admin</b>LTE</a>
         </div>
         <!-- /.login-logo -->
         <div class="card">
             <div class="card-body login-card-body">
-                <p class="login-box-msg">Sign in to start your session</p>
+                <p class="login-box-msg">You forgot your password? Here you can easily retrieve a new password.</p>
 
-                <form action="../../index3.html" method="post">
+                <form action="recover-password.php" method="post">
                     <div class="input-group mb-3">
                         <input type="email" class="form-control" placeholder="Email">
                         <div class="input-group-append">
@@ -36,44 +36,16 @@
                             </div>
                         </div>
                     </div>
-                    <div class="input-group mb-3">
-                        <input type="password" class="form-control" placeholder="Password">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
-                            </div>
-                        </div>
-                    </div>
                     <div class="row">
-                        <div class="col-8">
-                            <div class="icheck-primary">
-                                <input type="checkbox" id="remember">
-                                <label for="remember">
-                                    Remember Me
-                                </label>
-                            </div>
-                        </div>
-                        <!-- /.col -->
-                        <div class="col-4">
-                            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-primary btn-block">Request new password</button>
                         </div>
                         <!-- /.col -->
                     </div>
                 </form>
 
-<<<<<<< HEAD
-      <p class="mb-1">
-        <a href="forgot_password.php">I forgot my password</a>
-      </p>
-      <p class="mb-0">
-        <a href="register.html" class="text-center">Register a new membership</a>
-      </p>
-=======
-
-                <!-- /.social-auth-links -->
-
-                <p class="mb-1">
-                    <a href="forgot-password.php">I forgot my password</a>
+                <p class="mt-3 mb-1">
+                    <a href="login.php">Login</a>
                 </p>
                 <p class="mb-0">
                     <a href="register.php" class="text-center">Register a new membership</a>
@@ -81,7 +53,6 @@
             </div>
             <!-- /.login-card-body -->
         </div>
->>>>>>> 28876ec (wlululu)
     </div>
     <!-- /.login-box -->
 
