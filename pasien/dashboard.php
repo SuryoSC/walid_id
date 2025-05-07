bdd2b3c080f657bb761ce4640bb873aed832f776
Aldyaz1468 <aldyaz237629@smkn1kandeman.sch.id>
2025-05-07 15:13:06 +0700

    sekarep

---
+++ pasien/dashboard.php
@@ -0,0 +1,376 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <title>Walid.id | Situs Resmi Walid Life Center (Official Website)</title>
+    <style>
+
+    * {
+      font-family:"Josefin Sans", sans-serif;
+    }
+
+    a {
+      text-decoration: none;
+      color: black;
+      font-size: 17px;
+    }
+
+    p {
+      text-decoration: none;
+      color: white;
+      font-size: 17px;
+    }
+
+    .mjudul span {
+      color: white;
+      font-size: 20px;
+    }
+
+    .mfeat a {
+      color: white;
+      font-size: 17px;
+      padding-left: 20px;
+      text-decoration: none;
+    }
+
+    .runningtext span {
+      color: white;
+      font-size: 15px;
+    }
+
+    .sliderdoc span {
+      color: white;
+      font-size: 25px;
+    }
+
+    .kemitraan span {
+      color: black;
+      font-size: 25px;
+    }
+
+    .acontainer {
+      height: 1000px;
+      width: 90%;
+      margin-left: 5%;
+      margin-right: 5%;
+    }
+
+    .navtas {
+      background-color: #00BFFF;
+      height: 45px;
+      width: 100%;
+    }
+
+    .input-group-sm {
+      border-radius: 100%;
+    }
+
+    .navbaw {
+      background-color: white;
+      height: 60px;
+      padding-right: 50px;
+      display: flex;
+      justify-content: right;
+      align-items: center;
+      gap: 50px;
+    }
+
+    .bgutama {
+        width: 100%;
+    }
+
+    .bgutama img{
+      width: 100%;
+      height: 250px;
+      margin-bottom:10px;
+    }
+
+    .menu1 {
+      display: flex;
+      flex-direction: row;
+    }
+
+    .m1kan {
+      display: flex;
+      flex-direction: column;
+      gap: 1px;
+      width: 300px;
+
+    }
+
+    .features {
+      width: 100%;
+      height: 50px;
+      background-color: #ADFF2F;
+      display: flex;
+      text-align: center;
+      align-items: center;
+      justify-content: center;
+      margin-bottom: 2px;
+    }
+
+    .f1 {
+      width: 100%;
+      height: 45px;
+      background-color: #00BFFF;
+      display: flex;
+      text-align: center;
+      align-items: center;
+      margin-bottom: 2px;
+    }
+
+    .f2 {
+      width: 100%;
+      height: 45px;
+      background-color: #55CCFF;
+      display: flex;
+      text-align: center;
+      align-items: center;
+      margin-bottom: 2px;
+    }
+
+    .f3 {
+      width: 100%;
+      height: 45px;
+      background-color: #00BFFF;
+      display: flex;
+      text-align: center;
+      align-items: center;
+      margin-bottom: 2px;
+    }
+
+    .f4 {
+      width: 100%;
+      height: 45px;
+      background-color: #55CCFF;
+      display: flex;
+      text-align: center;
+      align-items: center;
+      border-bottom: 2px white;
+    }
+
+    .m1kir img {
+      width: 850px;
+      height: 239px;
+      margin-left: 10px;
+    }
+
+    .runningtext {
+      width: 100%;
+      height: 30px;
+      background-color: #00BFFF;
+      margin-top: 10px;
+      margin-bottom: 10px;
+      display: flex;
+      text-align: center;
+      align-items: center;
+      justify-content: center;
+    }
+
+    .sliderdoc {
+      display: flex;
+      flex-direction: row;
+    }
+    .sliderdoc img {
+      width: 100%;
+      height: 300px;
+    }
+
+    .doc1 {
+      width: 250px;
+      height: 300px;
+      border-radius: 5%;
+      border: 1px solid grey;
+    }
+
+    .doc2 {
+      width: 250px;
+      height: 300px;
+      border-radius: 5%;
+      border: 1px solid grey;
+    }
+
+    .doc3 {
+      width: 250px;
+      height: 300px;
+      border-radius: 5%;
+      border: 1px solid grey;
+    }
+
+    .doc4 {
+      width: 250px;
+      height: 300px;
+      border-radius: 5%;
+      border: 1px solid grey;
+    }
+
+    .doc5 {
+      width: 250px;
+      height: 300px;
+      border-radius: 5%;
+      border: 1px solid grey;
+    }
+
+    .kemitraan {
+      display: flex;
+      flex-direction: column;
+      margin-top: 10px;
+    }
+
+    .kemitraan img {
+      width:500px;
+    }
+
+    .kemtas {
+      display: flex;
+      justify-content: center;
+    }
+    .kembaw {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .footas {
+      background-color: #ADFF2F;
+      height: 45px;
+      width: 100%;
+      margin-bottom: 10px;
+      margin-top: 10px;
+    }
+
+    .footbaw {
+      background-color: #00BFFF;
+      height: 100px;
+      width: 100%;
+    }
+
+    .fbtas {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      gap: 20px;
+    }
+
+    .fbwah {
+      display: flex;
+      justify-content: center;
+    }
+
+    </style>
+</head>
+
+<body>
+    <div class="acontainer">
+
+      <div class="anavbar">
+          <div class="navtas">
+
+            </div>
+          <div class="navbaw">
+            <a href="#s1"><b>BERANDA</b></a>
+            <a href="#s1"><b>TENTANG</b></a>
+            <a href="#s1"><b>LAYANAN</b></a>
+            <a href="#s1"><b>SARAN & PENGADUAN</b></a>
+          </div>
+      </div>
+
+      <div class="bgutama">
+        <img src="../assets/img/bgwallid21.jpg" alt="">
+      </div>
+
+      <div class="menu1">
+        <div class="m1kan">
+          <div class="mjudul">
+            <div class="features">
+              <span><b>FITUR</b></span>
+            </div>
+          </div>
+          <div class="mfeat">
+            <div class="f1">
+                <a href="#s1"><b>PENDAFTARAN ONLINE</b></a>
+              </div>
+                <div class="f2">
+                <a href="#s1"><b>LIHAT NOMOR ANTREAN</b></a>
+              </div>
+                <div class="f3">
+                <a href="#s1"><b>JADWAL DOKTER</b></a>
+              </div>
+              <div class="f4">
+                <a href="#s1"><b>RIWAYAT PERIKSA PASIEN</b></a>
+              </div>
+            </div>
+          </div>
+          <div class="m1kir">
+          <img src="../assets/img/poster-satu.jpg" alt="">
+        </div>
+      </div>
+
+      <div class="runningtext">
+        <span><b>SELAMAT DATANG DI WALID.ID | PENDAFTARAN ONLINE DAPAT DILAKUKAN SEHARI SEBELUMNYA MULAI PUKUL 13.00 WIB <!-- | SEMOGA HARIMU MENYENANGKAN  --></b></span>
+      </div>
+
+      <div class="sliderdoc">
+        <div class="doc1">
+          <img src="../assets/img/dr-Suryo_Saputro.png" alt="">
+          <span><b>dr. Suryo</b></span>
+          <p></p>
+        </div>
+        <div class="doc2">
+          <img src="../assets/img/dr-Aldyaz_Budi_P.png" alt="">
+          <span><b>dr. Aldyaz</b></span>
+          <p></p>
+        </div>
+        <div class="doc3">
+          <img src="../assets/img/dr-Ezar_Fausta_GP.png" alt="">
+          <span><b>dr. Ezar</b></span>
+          <p></p>
+        </div>
+        <div class="doc4">
+          <img src="../assets/img/dr-Muhammad_Syarif__Hidayat.png" alt="">
+          <span><b>dr. Syarif</b></span>
+          <p></p>
+        </div>
+        <div class="doc5">
+          <img src="../assets/img/dr-Muhammad_Fakhri_Bintang_Pratama.png" alt="">
+          <span><b>dr. Fakhri</b></span>
+          <p></p>
+        </div>
+      </div>
+
+
+      <div class="kemitraan">
+        <div class="kemtas">
+          <span><b>PARTNERSHIP</b></span>
+        </div>
+        <div class="kembaw">
+        <a href=""><img src="../assets/img/Ministry of Health of the republic of indonesia.png" alt=""></a>
+        <a href=""><img src="../assets/img/Logo World Health Organization.png" alt=""></a>
+        </div>
+      </div>
+
+      <div class="afooter">
+        <div class="footas">
+
+        </div>
+        <div class="footbaw">
+          <div class="fbtas">
+            <p>Privacy Policy</p>
+            <p>Terms of Service</p>
+          </div>
+          <div class="fbwah">
+            <p>This website is © 2025 Walid Life Center. All rights reserved</p>
+          </div>
+        </div>
+      </div>
+    </div>
+</body>
+
+</html>
