e7d86c5452fad28a4c97c89d648ae410c4664fa7
ezrfst <ezar237644@student.smkn1kandeman.sch.id>
2025-05-15 15:31:55 +0700

    keolp

---
+++ pasien/pengaduan.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Saran & Pengaduan</title>
+</head>
+
+<body>
+    <div class="container">
+        <nav>
+        </nav>
+        <main>
+            <h1>Formulir Keluhan User Walid Life Center</h1>
+
+            <form action="">
+                <span>Identitas Diri</span>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Nama Lengkap</label>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Nama">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Nama Lengkap</label>
+                    <input type="radio" class="form-control" id="exampleFormControlInput1" placeholder="Laki-laki">
+                    <input type="radio" class="form-control" id="exampleFormControlInput1" placeholder="Perempuan">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlTextarea1" class="form-label" columns="8"> </label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="8"></textarea>
+                </div>
+
+            </form>
+        </main>
+    </div>
+</body>
+
+</html>
