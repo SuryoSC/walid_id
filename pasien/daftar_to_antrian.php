ff37e72767f29a264e379028091433e98c3ef40a
Syareeeeeeep <syarifhidayatt81@gmail.com>
2025-05-20 19:03:20 +0700

    qwerty

--- pasien/daftar_to_antrian.php
+++ pasien/daftar_to_antrian.php
@@ -38,6 +38,7 @@
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
     <style>
         * {
@@ -47,6 +48,7 @@
 </head>
 
 <body class="w-full flex flex-col items-center bg-gray-200">
+    <?php include 'layout/header.html'; ?>
 
     <!-- <h4><?= $nama ?></h4>
     <h4><?= $email ?></h4> -->
