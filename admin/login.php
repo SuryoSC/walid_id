cbc10ce3228c002aa1eb723a0c1cd9221991787c
Syareeeeeeep <syarifhidayatt81@gmail.com>
2025-06-03 19:55:14 +0700

    binggunggg

--- admin/login.php
+++ admin/login.php
@@ -43,6 +43,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Sebagai Admin</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
@@ -85,5 +86,15 @@
             </form>
         </div>
     </div>
+
+    <?php if($login_message === "gagal") : ?>
+       <script>
+            Swal.fire({
+                icon: 'error',
+                title: 'Gagal!',
+                text: 'Data salah, gagal login sebagai admin.'
+            });
+       </script>
+    <?php endif; ?>
 </body>
 </html>
